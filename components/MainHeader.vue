5a8eec8bf4ed72e79d33b8e3063a4d262dcd885d
tanihata <tanihata-katsuki@ksc.co.jp>
2022-05-19 09:39:24 +0900

    ヘッダー作成

---
+++ components/MainHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-header d-flex align-center">
+    <div class="header-label pl-2">タスク管理アプリ</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component({name: "mainHeader"})
+export default class extends Vue {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.main-header
+  height 100px !important
+  background-color #ccc
+
+.header-label
+  font-size 30px
+</style>
